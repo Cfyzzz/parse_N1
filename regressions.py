2057850cb23fe3c272748591a63cdad6a42fd623
Alexander Nedovizin <parkit74@gmail.com>
2020-09-13 15:47:03 +0500

    feat: три города на одном графике

--- regressions.py
+++ regressions.py
@@ -1,26 +1,18 @@
 import pandas as pd
 from sqlalchemy import create_engine
-from sklearn.datasets import make_blobs
-from sklearn.cluster import KMeans
+from matplotlib import pyplot
 
-
-features, _ = make_blobs(n_samples=8886,
-                         n_features=4,
-                         centers=2,
-                         random_state=1
-                         )
 
 database_connection = create_engine('sqlite:///realty.sqlite3')
-dataframe = pd.read_sql_query('SELECT price, area, rooms, sqm FROM apartments WHERE city like "Челябинск%"', database_connection)
+dataframe = pd.read_sql_query('SELECT price, area, rooms, sqm FROM apartments WHERE city like "Копе%"', database_connection)
+dataframe2 = pd.read_sql_query('SELECT price, area, rooms, sqm FROM apartments WHERE city like "Челябинск%"', database_connection)
+dataframe3 = pd.read_sql_query('SELECT price, area, rooms, sqm FROM apartments WHERE city like "Екатеринбург%"', database_connection)
 print(dataframe.head(5))
 
-clusterer = KMeans(2, random_state=0)
-clusterer.fit(features)
+fig, ax = pyplot.subplots()
 
-dataframe['cluster'] = clusterer.predict(features)
+dataframe3.plot(ax=ax, x='area', y='price', kind='scatter', color='red', label='Екатеринбург')
+dataframe2.plot(ax=ax, x='area', y='price', kind='scatter', color='green', label='Челябинск')
+dataframe.plot(ax=ax, x='area', y='price', kind='scatter', color='blue', label='Копейск')
 
-ax = dataframe.plot(x='sqm', y='cluster', kind='scatter')
-fig = ax.get_figure()
-
-
-fig.savefig('demo2.png', bbox_inches='tight')
+pyplot.savefig('demo2.png', bbox_inches='tight')
