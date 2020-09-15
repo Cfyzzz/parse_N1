edf0d5ee7afb3f114bdd57d704538b8fb2a95d92
Alexander Nedovizin <parkit74@gmail.com>
2020-09-15 23:06:18 +0500

    feat: группировка по материалу

--- regressions.py
+++ regressions.py
@@ -1,3 +1,5 @@
+import random
+
 import pandas as pd
 from sqlalchemy import create_engine
 from matplotlib import pyplot
@@ -11,26 +13,34 @@ def add_trend_by_x(ax, df, x, y, label="fit"):
 
 
 set_price = 10000000
-set_area = 150
+set_area = 250
 
 database_connection = create_engine('sqlite:///realty.sqlite3')
-dataframe = pd.read_sql_query(f'SELECT price, area, rooms, sqm FROM apartments WHERE city like "Копе%" AND price <= {set_price} AND area <= {set_area}', database_connection)
-dataframe2 = pd.read_sql_query(f'SELECT price, area, rooms, sqm FROM apartments WHERE city like "Челябинск%" AND price <= {set_price} AND area <= {set_area}', database_connection)
-dataframe3 = pd.read_sql_query(f'SELECT price, area, rooms, sqm FROM apartments WHERE city like "Екатеринбург%" AND price <= {set_price} AND area <= {set_area}', database_connection)
-dataframe4 = pd.read_sql_query(f'SELECT price, area, rooms, sqm FROM apartments WHERE city like "Казань%" AND price <= {set_price} AND area <= {set_area}', database_connection)
-print(dataframe.head(5))
+# dataframe = pd.read_sql_query(f'SELECT price, area, rooms, sqm FROM apartments WHERE city like "Копе%" AND price <= {set_price} AND area <= {set_area}', database_connection)
+# dataframe2 = pd.read_sql_query(f'SELECT price, area, rooms, sqm FROM apartments WHERE city like "Челябинск%" AND price <= {set_price} AND area <= {set_area}', database_connection)
+dataframe3 = pd.read_sql_query(f'SELECT price, area, rooms, sqm, material FROM apartments WHERE city like "Екатеринбург%" AND price <= {set_price} AND area <= {set_area}', database_connection)
+# dataframe4 = pd.read_sql_query(f'SELECT price, area, rooms, sqm FROM apartments WHERE city like "Казань%" AND price <= {set_price} AND area <= {set_area}', database_connection)
+# print(dataframe.head(5))
 
+dataframes = dataframe3.groupby('material')
 
 fig, ax = pyplot.subplots()
 
-add_trend_by_x(ax=ax, df=dataframe3, x='area', y='price', label='fit Екатеринбург')
-add_trend_by_x(ax=ax, df=dataframe2, x='area', y='price', label='fit Челябинск')
-add_trend_by_x(ax=ax, df=dataframe, x='area', y='price', label='fit Копейск')
-add_trend_by_x(ax=ax, df=dataframe4, x='area', y='price', label='fit Казань')
+# add_trend_by_x(ax=ax, df=dataframe3, x='material', y='price', label='fit Екатеринбург')
+# add_trend_by_x(ax=ax, df=dataframe2, x='area', y='price', label='fit Челябинск')
+# add_trend_by_x(ax=ax, df=dataframe, x='area', y='price', label='fit Копейск')
+# add_trend_by_x(ax=ax, df=dataframe4, x='area', y='price', label='fit Казань')
 
-dataframe3.plot(ax=ax, x='area', y='price', kind='scatter', color='red', label='Екатеринбург')
-dataframe2.plot(ax=ax, x='area', y='price', kind='scatter', color='green', label='Челябинск')
-dataframe.plot(ax=ax, x='area', y='price', kind='scatter', color='blue', label='Копейск')
-dataframe4.plot(ax=ax, x='area', y='price', kind='scatter', color='yellow', label='Казань')
+number_of_colors = 12
+def random_color():
+    color = list(map(lambda x: x/255, list(random.choices(range(256), k=3))))
+    print(color)
+    return color
+
+for idx, df in enumerate(dataframes):
+    df[1].plot(ax=ax, x='area', y='price', kind='scatter', color=random_color(), label=df[0])
+# dataframe2.plot(ax=ax, x='area', y='price', kind='scatter', color='green', label='Челябинск')
+# dataframe.plot(ax=ax, x='area', y='price', kind='scatter', color='blue', label='Копейск')
+# dataframe4.plot(ax=ax, x='area', y='price', kind='scatter', color='yellow', label='Казань')
 
 pyplot.savefig('demo2.png', bbox_inches='tight')
